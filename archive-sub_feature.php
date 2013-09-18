3770308bf2e3ca39275deea4431d0b2d91fc1aea
Josh <JPollock412@gmail.com>
2013-09-18 04:29:50 -0400

    create archive-sub_feature from saveas archive-feature

---
+++ archive-sub_feature.php
@@ -0,0 +1,46 @@
+<?php
+/** archive.php
+ *
+ * The template for displaying Archive pages.
+ *
+ * Used to display archive-type pages if nothing more specific matches a query.
+ * For example, puts together date-based pages if no date.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @author		Konstantin Obenland
+ * @package		The Bootstrap
+ * @since		1.0.0 - 07.02.2012
+ */
+
+get_header(); ?>
+
+<section id="primary" class="span12">
+
+	<?php tha_content_before(); ?>
+	<div id="content" role="main">
+		<?php tha_content_top();
+		
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<h1 class="page-title">
+					<?php
+						_e( 'All Features', 'the-bootstrap' );
+					endif; ?>
+				</h1>
+			</header><!-- .page-header -->
+
+			<?php
+			jp_feature_archive_loop();
+		tha_content_bottom(); ?>
+	</div><!-- #content -->
+	<?php tha_content_after(); ?>
+</section><!-- #primary -->
+
+<?php
+get_footer();
+
+
+/* End of file archive.php */
+/* Location: ./wp-content/themes/the-bootstrap/archive.php */
